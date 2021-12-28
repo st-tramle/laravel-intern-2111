b26c9bb397df86bfeeb7596e62e6fe12436c798d
st-tramle <tram.le@supremetech.vn>
2021-12-28 22:35:11 +0700

    API return list tasks (id, title, description)

---
+++ application/app/Http/Controllers/Api/TaskController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\TaskResource;
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return TaskResource::collection(Task::all());
+    }
+}
